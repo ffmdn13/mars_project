db1ded57ca7e3d847e27a144c42c1a554759b5fa
ffmdn13 <fafasnakes314@gmail.com>
2023-05-30 11:18:38 +0700

    First

--- app.py
+++ app.py
@@ -1,45 +1,13 @@
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, jsonify
-from pymongo import MongoClient
-
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-
-MONGODB_URI = os.environ.get('MONGODB_URI')
-DB_NAME = os.environ.get('DB_NAME')
-
-client = MongoClient(MONGODB_URI)
-db = client[DB_NAME]
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET'])
-def home():
+@app.route('/')
+def main():
     return render_template('index.html')
 
-@app.route('/mars', methods=['POST'])
-def post_mars():
-    name_receive = request.form['name_give']
-    address_receive = request.form['address_give']
-    size_receive = request.form['size_give']
-
-    doc = {
-        'name': name_receive,
-        'address': address_receive,
-        'size': size_receive
-    }
-
-    db.mars.insert_one(doc)
-
-    return jsonify({'msg': 'complete!'})
-
-@app.route('/mars', methods = ['GET'])
-def get_mars():
-    orders_list = list(db.mars.find({}, {'_id': False}))
-    return jsonify({'orders': orders_list})
-    
-
 if __name__ == '__main__':
-    app.run('0.0.0.0', port = 5000, debug = True)
+    app.run('0.0.0.0', port=5000, debug=True)
